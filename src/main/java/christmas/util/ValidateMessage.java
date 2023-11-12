ff5b3bd13b9ad6575060dfbc45ab68f609a417a7
inhoru126 <inhoru126@gmail.com>
2023-11-13 04:23:34 +0900

    fix : ERROR메시지 추가

--- src/main/java/christmas/util/ValidateMessage.java
+++ src/main/java/christmas/util/ValidateMessage.java
@@ -2,7 +2,9 @@ package christmas.util;
 
 public class ValidateMessage {
 
-    public static final String INPUT_DATE_TYPE = "^([1-9]|1\\d|2[0-9]|3[0-1])$";
-
     public static final String ERROR_DATE_MESSAGE = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+    public static final String ERROR_MENU_MESSAGE = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
+    public static final String ERROR_MAX_ORDER_MESSAGE = "[ERROR] 메뉴는 총 20개 이하만 주문이 가능 합니다.";
+    public static final String ERROR_ZERO_ORDER_MESSAGE = "[ERROR] 0개는 입력 주문 불가능 합니다.";
+    public static final String ERROR_BEVERAGE_MESSAGE = "[ERROR] 음료만 주문 시, 주문할 수 없습니다.";
 }
