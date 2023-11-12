e244569e91243c8e04eba4b56a289b892b1bb844
inhoru126 <inhoru126@gmail.com>
2023-11-13 03:52:05 +0900

    fix : DateTets 클래스이름변경

--- src/test/java/christmas/domain/DateTest.java
+++ src/test/java/christmas/domain/DateTest.java
@@ -6,13 +6,13 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-public class OrderDateTest {
+public class DateTest {
 
     @DisplayName("잘못된 유형 날짜 입력 테스트")
     @ParameterizedTest
     @ValueSource(strings = {"0", "32", "77", "asd", "-3", "!@#"})
     void testOrderDateOutOfRange(String input) {
-        assertThatThrownBy(() -> new OrderDate(input))
+        assertThatThrownBy(() -> new Date(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
