915e92fe9cc89b9d4842da142c2b98e55a4093a2
inhoru126 <inhoru126@gmail.com>
2023-11-13 04:19:33 +0900

    docs : 기능 목록 갱신

---
+++ src/main/java/christmas/util/Const.java
@@ -0,0 +1,12 @@
+package christmas.util;
+
+public class Const {
+
+    public static final String INPUT_DATE_TYPE = "^([1-9]|1\\d|2[0-9]|3[0-1])$";
+    public static final String INPUT_MENU_TYPE = "^[^-]+-\\d+$";
+    public static final int ZERO = 0;
+    public static final int ONE = 1;
+    public static final int TWENTY = 20;
+
+
+}
